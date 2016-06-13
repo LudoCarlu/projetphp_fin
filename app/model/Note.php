8744a966ab68110dcf66aaa1731e98212d00ae8d
ludoviccarlu <ludoviccarlu@gmail.com>
2016-06-13 09:57:07 -0400

    Note fini sur un album

--- app/model/Note.php
+++ app/model/Note.php
@@ -20,14 +20,40 @@ class Note extends Model {
   
   public static function ajouterNote($idAl,$idU,$note) {
     $db = Database::getInstance();
-    $sql = "INSERT INTO note (idAl,idU,note) VALUES (:idAl,:idU,:note)";
-    $stmt = $db->prepare($sql);
-    $stmt->setFetchMode(PDO::FETCH_CLASS, "Note");
-    return $stmt->execute(array(
-      ':idAl' => $idAl,
-      ':idU' => $idU,
-      ':note' =>$note
-    ));
+		$sql = "SELECT * FROM note WHERE idAl=".$idAl." AND idU=".$idU;
+		$stmt = $db->query($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Note");
+		
+		if($res = $stmt->fetch()) {
+			$db = Database::getInstance();
+			$sql2 = "UPDATE note SET note=$note WHERE idU=$idU AND idAl=$idAl";
+			$stmt = $db->query($sql2);
+			$stmt->setFetchMode(PDO::FETCH_CLASS, "Note");
+		}
+		else {
+			$db = Database::getInstance();
+			$sql2 = "INSERT INTO note (idAl,idU,note) VALUES (:idAl,:idU,:note)";
+			$stmt = $db->prepare($sql2);
+			$stmt->setFetchMode(PDO::FETCH_CLASS, "Note");
+			return $stmt->execute(array(
+				':idAl' => $idAl,
+				':idU' => $idU,
+				':note' =>$note
+			));
+		}
   }
+	public static function CalculMoyFromId ($idAl) {
+		$db = Database::getInstance();
+		$sql = "SELECT AVG(note) as note FROM note WHERE idAl=".$idAl;
+		$stmt = $db->query($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Note");
+		$res=$stmt->fetch();
+		
+		$sql = "UPDATE album SET note=$res->note WHERE idAl=$idAl";
+		$stmt = $db->query($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Note");
+		
+	}
+	
 }
 ?>
