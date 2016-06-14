76ebc1a991801fdde4bbb412bfeca4660d915758
vkwan-teau <vkwan-teau@hotmail.fr>
2016-06-14 13:02:00 -0400

    correct

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -6,11 +6,8 @@ class IndexController extends Controller {
       $this->view->palmares = Album::palmares();
       $this->view->listeArtiste = Artiste::getList();
       $this->view->listeGenre = Album::getListGenre();
-<<<<<<< HEAD
       $this->view->utilisateur = Utilisateur::getList();
-=======
       $this->view->listeNote = Note::listeNoteAlbum();
->>>>>>> bc69335eba42791a9ad811951c1cef86d846ef0b
       $this->view->display();
    }
 
